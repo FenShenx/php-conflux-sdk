5557b8e1067760cab6926bc57289fe62a41ce338
分神 <qiao@qnt0.com>
2022-10-19 17:32:07 +0800

    HttpProvider test code

---
+++ test/unit/Providers/HttpProvidersTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Test\Unit\Providers;
+
+use Fenshenx\PhpConfluxSdk\Providers\HttpProvider;
+use Test\TestCase;
+
+final class HttpProvidersTest extends TestCase
+{
+    public function testSend()
+    {
+        $provider = new HttpProvider($this->testHost);
+        $res = $provider->send('cfx_clientVersion', []);
+
+        $this->assertSame($res['jsonrpc'], $provider->getJsonRpcVersion());
+    }
+
+    public function testCall()
+    {
+        $provider = new HttpProvider($this->testHost);
+        $res = $provider->send('cfx_clientVersion', []);
+
+        $this->assertSame($res['jsonrpc'], $provider->getJsonRpcVersion());
+    }
+
+    public function testBatch()
+    {
+        $provider = new HttpProvider($this->testHost);
+
+        $arr = [
+            ['method' => 'cfx_clientVersion', 'params' => []],
+            ['method' => 'cfx_clientVersion', 'params' => []],
+        ];
+
+        $resArr = $provider->batch($arr);
+
+        foreach ($resArr as $item) {
+            $this->assertSame($item['jsonrpc'], $provider->getJsonRpcVersion());
+        }
+    }
+}
