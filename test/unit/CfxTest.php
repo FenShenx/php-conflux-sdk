fbec065c72f88e5e6bdf9b3c3cc6281c7aefb09a
分神 <qiao@qnt0.com>
2022-10-25 17:21:12 +0800

    epochNumber test code

--- test/unit/CfxTest.php
+++ test/unit/CfxTest.php
@@ -29,6 +29,25 @@ class CfxTest extends TestCase
         $this->assertInstanceOf(BigInteger::class, $res);
     }
 
+    public function testEpochNumber()
+    {
+        $cfx = $this->getCfx();
+
+        $tag1 = 97011356;
+        $res1 = $cfx->epochNumber($tag1);
+        $this->assertSame($tag1, (int)$res1->toString());
+
+        $paramsTags = [
+            'earliest', 'latest_checkpoint', 'latest_finalized', 'latest_confirmed', 'latest_state', 'latest_mined'
+        ];
+
+        foreach ($paramsTags as $tag) {
+
+            $res = $cfx->epochNumber($tag);
+            $this->assertInstanceOf(BigInteger::class, $res);
+        }
+    }
+
     private function getCfx()
     {
         $conflux = new Conflux($this->testHost, $this->networkId);
