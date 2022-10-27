2122365912bf22df4888f5dc08810766c51a1c9e
分神 <qiao@qnt0.com>
2022-10-27 17:44:07 +0800

    PrivateKey2PublicKey util

--- src/Utils/SignUtil.php
+++ src/Utils/SignUtil.php
@@ -2,10 +2,13 @@
 
 namespace Fenshenx\PhpConfluxSdk\Utils;
 
+use Elliptic\EC;
 use kornrunner\Keccak;
 
 class SignUtil
 {
+    private static EC $secp256k1;
+
     private function __construct() {}
 
     public static function getRandomBytes($size)
@@ -19,5 +22,26 @@ class SignUtil
         $middle = self::getRandomBytes(32).$inner.self::getRandomBytes(32);
 
         return Keccak::hash($middle, 256);
+    }
+
+    public static function privateKey2PublicKey($privateKey)
+    {
+        $privateKey = FormatUtil::stripZero($privateKey);
+
+        if (strlen($privateKey) !== 64)
+            throw new \Exception('Invalid private key length.');
+
+        self::initSecp256k1();
+
+        $privateKey = self::$secp256k1->keyFromPrivate($privateKey, 'hex');
+        $publicKey = $privateKey->getPublic(false, 'hex');
+
+        return '0x' . $publicKey;
+    }
+
+    private static function initSecp256k1()
+    {
+        if (empty(self::$secp256k1))
+            self::$secp256k1 = new EC("secp256k1");
     }
 }
