bebea27af99be36eefb5e5d44274432fafa50c04
分神 <qiao@qnt0.com>
2022-11-08 16:31:28 +0800

    HexFormatter add Drip zero digits format

--- src/Formatters/HexFormatter.php
+++ src/Formatters/HexFormatter.php
@@ -2,6 +2,7 @@
 
 namespace Fenshenx\PhpConfluxSdk\Formatters;
 
+use Fenshenx\PhpConfluxSdk\Drip;
 use Fenshenx\PhpConfluxSdk\Utils\FormatUtil;
 
 class HexFormatter implements IFormatter
@@ -9,6 +10,17 @@ class HexFormatter implements IFormatter
 
     public static function format($value)
     {
+        if ($value instanceof Drip) {
+
+            $valueHex = FormatUtil::stripZero($value->getDripHex());
+
+            // remove zero digits
+            if (str_starts_with($valueHex, '0'))
+                $valueHex = substr($valueHex, 1);
+
+            return FormatUtil::zeroPrefix($valueHex);
+        }
+
         return FormatUtil::numberToHex($value, true);
     }
 }
