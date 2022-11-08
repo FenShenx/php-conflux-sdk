ebbb61288492962bf1c6109ddc50b71eba600cb8
分神 <qiao@qnt0.com>
2022-11-08 16:41:09 +0800

    EstimateGasAndCollateral method

---
+++ src/Methods/EstimateGasAndCollateral.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Fenshenx\PhpConfluxSdk\Methods;
+
+use Fenshenx\PhpConfluxSdk\Formatters\DripFormatter;
+use Fenshenx\PhpConfluxSdk\Formatters\EpochNumberFormatter;
+use Fenshenx\PhpConfluxSdk\Formatters\HexFormatter;
+
+class EstimateGasAndCollateral extends BaseMethod
+{
+    protected string $methodName = 'cfx_estimateGasAndCollateral';
+
+    protected array $requestFormatters = [
+        0 => [
+            'data' => 'StringHexFormatter',
+            'gas' => HexFormatter::class,
+            'gasPrice' => HexFormatter::class,
+            'value' => HexFormatter::class,
+            'nonce' => HexFormatter::class,
+        ],
+        1 => EpochNumberFormatter::class
+    ];
+
+    protected array $responseFormatters = [
+        'gasLimit' => DripFormatter::class,
+        'gasUsed' => DripFormatter::class,
+        'storageCollateralized' => DripFormatter::class
+    ];
+
+    protected function validate($params)
+    {
+
+    }
+}
