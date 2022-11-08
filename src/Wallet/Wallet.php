c85ae2048d5700b29816236ab809299cbae0d020
分神 <qiao@qnt0.com>
2022-11-08 17:50:25 +0800

    wallet

--- src/Wallet/Wallet.php
+++ src/Wallet/Wallet.php
@@ -4,6 +4,12 @@ namespace Fenshenx\PhpConfluxSdk\Wallet;
 
 class Wallet
 {
+    /**
+     * [confluxAddress => Account]
+     * @var Account[]
+     */
+    private $accounts = [];
+
     public function __construct(
         private int $networkId
     )
@@ -14,5 +20,55 @@ class Wallet
     public function getNetworkId()
     {
         return $this->networkId;
+    }
+
+    public function has($address): bool
+    {
+        return isset($this->accounts[$address]);
+    }
+
+    public function delete($address): void
+    {
+        if ($this->has($address))
+            unset($this->accounts[$address]);
+    }
+
+    public function clear(): void
+    {
+        $this->accounts = [];
+    }
+
+    public function get($address): Account|null
+    {
+        if ($this->has($address))
+            return $this->accounts[$address];
+
+        return null;
+    }
+
+    public function addPrivateKey($privateKey): Account
+    {
+        $account = new Account($privateKey, $this->networkId);
+
+        if ($this->has($account->getConfluxAddress()))
+            return $this->get($account->getConfluxAddress());
+
+        $this->accounts[$account->getConfluxAddress()] = $account;
+
+        return $account;
+    }
+
+    public function addRandom(): Account
+    {
+        $account = Account::random($this->networkId);
+
+        $this->accounts[$account->getConfluxAddress()] = $account;
+
+        return $account;
+    }
+
+    public function addKeystore($keystore, $password)
+    {
+        //TODO
     }
 }
