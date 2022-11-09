4ab77507ed147e8c59fe92d40c6fc551b219741b
分神 <qiao@qnt0.com>
2022-11-09 15:31:47 +0800

    SendRawTransaction method

---
+++ src/Methods/SendRawTransaction.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Fenshenx\PhpConfluxSdk\Methods;
+
+class SendRawTransaction extends BaseMethod
+{
+    protected string $methodName = "cfx_sendRawTransaction";
+
+    protected function validate($params)
+    {
+
+    }
+}
