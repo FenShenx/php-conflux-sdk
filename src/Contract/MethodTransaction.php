51972ba1387724416e2b130d24e6a4368b807797
分神 <qiao@qnt0.com>
2022-11-14 17:56:16 +0800

    send no params & no sign contract method

--- src/Contract/MethodTransaction.php
+++ src/Contract/MethodTransaction.php
@@ -9,9 +9,12 @@ use Fenshenx\PhpConfluxSdk\Wallet\Account;
 
 class MethodTransaction
 {
+    private Account $signAccount;
+
     public function __construct(
         private Conflux $conflux,
         private ContractMethod $contractMethod,
+        private string $contractAddress,
         private array $args
     )
     {
@@ -20,12 +23,23 @@ class MethodTransaction
 
     public function send()
     {
+        //TODO: Sign data
 
+        $encodedData = $this->contractMethod->encodeInputs($this->args);
+
+        $res = $this->conflux->getCfx()->call([
+            'to' => $this->contractAddress,
+            'data' => $encodedData
+        ]);
+
+        return $this->contractMethod->decodeOutputs($res);
     }
 
     public function sendTransaction(Account $account)
     {
+        $this->signAccount = $account;
 
+        return $this;
     }
 
     public function estimateGasAndCollateral(Account $account, EpochNumber|String|int|null $epochNumber = null)
