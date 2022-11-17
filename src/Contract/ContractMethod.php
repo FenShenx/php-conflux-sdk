d4fb056a05405fc884c3e03f088fb47add48ad3f
分神 <qiao@qnt0.com>
2022-11-17 10:38:47 +0800

    Contract method encode inputs

--- src/Contract/ContractMethod.php
+++ src/Contract/ContractMethod.php
@@ -5,6 +5,7 @@ namespace Fenshenx\PhpConfluxSdk\Contract;
 use Fenshenx\PhpConfluxSdk\Contract\Coder\CoderFactory;
 use Fenshenx\PhpConfluxSdk\Contract\Coder\ICoder;
 use Fenshenx\PhpConfluxSdk\Contract\Coder\IntegerCoder;
+use Fenshenx\PhpConfluxSdk\Utils\EncodeUtil;
 use Fenshenx\PhpConfluxSdk\Utils\FormatUtil;
 use kornrunner\Keccak;
 use phpseclib3\Math\BigInteger;
@@ -47,7 +48,64 @@ class ContractMethod
         $data = $this->signature;
 
         if (!empty($inputs)) {
-            //TODO
+
+            $inputKeys = array_keys($inputs);
+            if ($inputKeys != range(0, count($inputs)-1)) {
+                //inputs is key => value array
+                //PHP8.0 Named Arguments https://www.php.net/manual/en/functions.arguments.php#functions.named-arguments
+                $inputCoderKeysIndex = array_flip(array_keys($this->inputsCoders));
+
+                $newInputs = [];
+                foreach ($inputs as $k => $v) {
+
+                    if (!isset($inputCoderKeysIndex[$k]))
+                        throw new \Exception("Undefined param".$k);
+
+                    $newInputs[$inputCoderKeysIndex[$k]] = $v;
+                }
+
+                //resort inputs array
+                ksort($newInputs);
+                $inputs = $newInputs;
+            }
+
+            //encode params
+            $staticList = [];
+            $dynamicList = [];
+            $offset = 0;
+
+            foreach (array_values($this->inputsCoders) as $k => $coder) {
+
+                if (!isset($inputs[$k]))
+                    break;
+
+                $value = $inputs[$k];
+                $buffer = $coder->encode($value);
+
+                if ($coder->getDynamic()) {
+
+                    $offset += EncodeUtil::WORD_BYTES;
+                    $staticList[] = new BigInteger(count($dynamicList));  // push index of dynamic to static
+                    $dynamicList[] = $buffer;
+                } else {
+
+                    $offset += strlen(hex2bin(FormatUtil::stripZero($buffer)));
+                    $staticList[] = $buffer;
+                }
+            }
+
+            foreach ($staticList as $k => $v) {
+                if ($v instanceof BigInteger) {
+                    $staticList[$k] = $this->integerCoder->encode($offset);
+                    $offset += strlen(hex2bin(FormatUtil::stripZero($dynamicList[(int)$v->toString()])));
+                }
+            }
+
+            $encoded = implode(array_map(function ($hex) {
+                return FormatUtil::stripZero($hex);
+            }, array_merge($staticList, $dynamicList)));
+
+            $data .= $encoded;
         }
 
         return FormatUtil::zeroPrefix($data);
